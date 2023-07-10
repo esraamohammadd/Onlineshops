aaa5903497671facf2bae016cdd8c63fbeab9eda
esraamohammadd <esraamohammad546@gmail.com>
2023-07-10 12:01:32 +0200

    second

---
+++ app/src/main/java/com/example/online_shop/sginup.java
@@ -0,0 +1,13 @@
+package com.example.online_shop;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class sginup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sginup);
+    }
+}
