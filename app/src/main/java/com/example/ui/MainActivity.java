df94b522495364768d4dd0da182d7345ba26899b
esraamohammadd <esraamohammad546@gmail.com>
2023-07-11 08:30:34 +0200

    3

---
+++ app/src/main/java/com/example/ui/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.example.online_shop.R;
+
+public class MainActivity extends AppCompatActivity {
+        TextView tv_sginUp;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+        tv_sginUp = findViewById(R.id.tv_sginUp);
+
+        tv_sginUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+
+            }
+        });
+    }
+}
