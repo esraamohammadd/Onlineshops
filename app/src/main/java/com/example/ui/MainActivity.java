e308050e97f06cf33f4656fa3470a56973a983a9
esraamohammadd <esraamohammad546@gmail.com>
2023-07-13 20:13:57 +0300

    4

--- app/src/main/java/com/example/ui/MainActivity.java
+++ app/src/main/java/com/example/ui/MainActivity.java
@@ -6,17 +6,27 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.example.online_shop.R;
 
 public class MainActivity extends AppCompatActivity {
-        TextView tv_sginUp;
+
+    TextView tv_sginUp;
+    EditText et_email, et_password;
+    Button btn_sginIn ;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getSupportActionBar().hide();
+
+        et_email = findViewById(R.id.et_sginemail);
+        et_password = findViewById(R.id.et_sginpassword);
+        btn_sginIn = findViewById(R.id.bt_sginIn);
         tv_sginUp = findViewById(R.id.tv_sginUp);
 
         tv_sginUp.setOnClickListener(new View.OnClickListener() {
@@ -28,5 +38,18 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+        btn_sginIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+
+            }
+        });
+
+
+
+
     }
 }
